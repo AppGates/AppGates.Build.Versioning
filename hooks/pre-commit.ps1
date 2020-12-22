7b6ca5205bb3e7ce18705004984f2ff18eb83f02
TomSun <tftomsun@web.de>
2020-12-22 11:19:42 +0100

    tets

--- hooks/pre-commit.ps1
+++ hooks/pre-commit.ps1
@@ -1,6 +1,25 @@
-& 'pre-commit2.ps1'
 
-If($LastExitcode -eq 0){
+echo $PSScriptRoot
+
+$pinfo = New-Object System.Diagnostics.ProcessStartInfo
+$pinfo.FileName = "pwsh.exe"
+$pinfo.RedirectStandardError = $false
+$pinfo.RedirectStandardOutput = $false
+$pinfo.UseShellExecute = $false
+$pinfo.Arguments = "$PSScriptRoot\pre-commit2.ps1"
+$p = New-Object System.Diagnostics.Process
+$p.StartInfo = $pinfo
+$p.Start() | Out-Null
+#Do Other Stuff Here....
+$p.WaitForExit()
+$exitCode = $p.ExitCode
+
+
+
+
+
+
+If($exitCode -eq 0){
 
 Write-Host 'Der Exit Code ist wie erwartet 0!' -ForegroundColor Green
 
@@ -9,4 +28,4 @@ Write-Host 'Der Exit Code ist wie erwartet 0!' -ForegroundColor Green
 Write-Host 'Der Exit Code ist NICHT 0!' -ForegroundColor Magenta
 }
 
-exit 1
+exit $exitCode
